342524036283f33ce38b5b5aad8d7ae0d900de2b
Chayannn <chayanp188@gmail.com>
2023-07-13 19:36:29 +0530

    updated

--- build/js/main.js
+++ build/js/main.js
@@ -5,8 +5,21 @@
 let multiply = function (c, d) {
     return c * d;
 };
-console.log(multiply(5, 5));
+// console.log(multiply(5, 5));
 // Optional Parameters
 const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a, b, c = 2) => {
     return a + b + c;
 };
+console.log(sumAll(5, 5, 7));
+console.log(sumAll(5, 7));
+// Rest Parameters
+const total = (a, ...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+console.log(total(1, 2, 3, 4));
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,43 @@ let multiply: mathFunction = function (c, d) {
   return c * d;
 };
 
-console.log(multiply(5, 5));
+// console.log(multiply(5, 5));
 
 // Optional Parameters
-const addAll = (a: number, b: number, c: number): number => {
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
+const sumAll = (a: number, b: number, c: number = 2): number => {
   return a + b + c;
+};
+
+console.log(sumAll(5, 5, 7));
+console.log(sumAll(5, 7));
+
+// Rest Parameters
+const total = (a: number, ...nums: number[]): number => {
+  return nums.reduce((prev, curr) => prev + curr);
+};
+
+console.log(total(1, 2, 3, 4));
+
+const createError = (errMsg: string): never => {
+  throw new Error(errMsg);
+};
+
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break;
+  }
+};
+
+const numberOrString = (value: number | string): string => {
+  if (typeof value === "string") return "string";
+  if (typeof value === "number") return "number";
+  return createError("This should never happen");
 };
