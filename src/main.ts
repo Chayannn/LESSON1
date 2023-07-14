29eb27cf770bbfa7c797a53506dfab7edab8f65d
Chayannn <chayanp188@gmail.com>
2023-07-14 11:32:20 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -54,41 +54,163 @@
 //   return createError("This should never happen");
 // };
 
-type One = string;
-type Two = string | number;
-type Three = "hello";
+// type One = string;
+// type Two = string | number;
+// type Three = "hello";
 
-// Convert to more or less specific
-let a: One = "hello";
-let b = a as Two; // Less specific
+// // Convert to more or less specific
+// let a: One = "hello";
+// let b = a as Two; // Less specific
 
-let c = a as Three; //More specific
+// let c = a as Three; //More specific
 
-let d = <One>"world";
-let e = <string | number>"world";
+// let d = <One>"world";
+// let e = <string | number>"world";
 
-const addOrConcat = (
-  a: number,
-  b: number,
-  c: "add" | "concat"
-): number | string => {
-  if (c === "add") return a + b;
-  return "" + a + b;
-};
+// const addOrConcat = (
+//   a: number,
+//   b: number,
+//   c: "add" | "concat"
+// ): number | string => {
+//   if (c === "add") return a + b;
+//   return "" + a + b;
+// };
 
-let myVal: string = addOrConcat(2, 2, "concat") as string;
+// let myVal: string = addOrConcat(2, 2, "concat") as string;
 
-// Be careful! TS sees no problem - but a string is returned
-let nextVal: number = addOrConcat(2, 2, "concat") as number;
+// // Be careful! TS sees no problem - but a string is returned
+// let nextVal: number = addOrConcat(2, 2, "concat") as number;
 
-// 10 as string;
-// Over ruling typescript
-10 as unknown as string;
+// // 10 as string;
+// // Over ruling typescript
+// 10 as unknown as string;
 
-// The Dom
-const img = document.querySelector("img")!;
-const myImg = document.getElementById("#img") as HTMLImageElement;
-const nextImg = <HTMLImageElement>document.getElementById("#img");
+// // The Dom
+// const img = document.querySelector("img")!;
+// const myImg = document.getElementById("#img") as HTMLImageElement;
+// const nextImg = <HTMLImageElement>document.getElementById("#img");
 
-img.src;
-myImg.src;
+// img.src;
+// myImg.src;
+
+// Chapter 6 Classes
+
+// class Coder {
+//   name: string;
+//   music: string;
+//   age: number;
+//   lang: string;
+
+//   constructor(name: string, music: string, age: number, lang: string) {
+//     this.name = name;
+//     this.music = music;
+//     this.age = age;
+//     this.lang = lang;
+//   }
+// }
+// class Coder {
+//   constructor(
+//     public readonly name: string,
+//     public music: string,
+//     private age: number,
+//     protected lang: string = "Typescript"
+//   ) {}
+//   public getAge() {
+//     return `Hello, I'm ${this.age}`;
+//   }
+// }
+// // const Dave = new Coder("Chayan", "R&B", 21, "English");
+// const Chayan = new Coder("Chayan", "R&B", 21);
+// console.log(Chayan.getAge());
+// // Valid JavaScript but not Typescript
+// // console.log(Chayan.age);
+// // console.log(Chayan.lang);
+// class WebDev extends Coder {
+//   constructor(
+//     public computer: string,
+//     name: string,
+//     music: string,
+//     age: number
+//   ) {
+//     super(name, music, age);
+//     this.computer = computer;
+//   }
+//   public getLang() {
+//     return `I write ${this.lang}`;
+//   }
+// }
+// const Sara = new WebDev("Linux", "Sara", "Lofi", 25);
+// console.log(Sara.getLang());
+// // console.log(Sara.age);
+// // console.log(Sara.lang);
+////////////////////////////////////////////
+// interface Musician {
+//   name: string;
+//   instrument: string;
+//   play(action: string): string;
+// }
+
+// class Guitarist implements Musician {
+//   name: string;
+//   instrument: string;
+//   constructor(name: string, instrument: string) {
+//     this.name = name;
+//     this.instrument = instrument;
+//   }
+//   play(action: string): string {
+//     return `${this.name} ${action} the ${this.instrument}`;
+//   }
+// }
+
+// const Page = new Guitarist("Jimmy", "Guitar");
+// console.log(Page.play("strums"));
+//////////////////////////////////////////////////////
+
+// class Peeps {
+//   static count: number = 0;
+
+//   static getCount(): number {
+//     return Peeps.count;
+//   }
+
+//   public id: number;
+
+//   constructor(public name: string) {
+//     this.name = name;
+//     this.id = ++Peeps.count;
+//   }
+// }
+
+// const John = new Peeps("John");
+// const Steve = new Peeps("Steve");
+// const Amy = new Peeps("Amy");
+
+// console.log(Peeps.count);
+// console.log(Steve.id);
+////////////////////////////////////////////////////////////
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else throw new Error("Param is not an Array of strings");
+  }
+}
+
+const MyBands = new Bands();
+
+MyBands.data = ["Neil Young", "Led Zep"];
+console.log(MyBands.data);
+
+MyBands.data = [...MyBands.data, "ZZ Top"];
+console.log(MyBands.data);
